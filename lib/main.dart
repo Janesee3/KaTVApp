f03de61946d3f667baed7442e3f3683feb8edcae
Jane See <seeloojane@gmail.com>
2018-12-05 11:02:11 +0800

    add ability to skip songs, delete songs and move song to top

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -39,53 +30,158 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(child: VideoComponent(videoUrl: "meow"), flex: 2),
-            Expanded(child: KtvUI(), flex: 3)
-          ],
-        )));
+    return AppInheritedWidget(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+            ),
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(child: VideoComponent(), flex: 2),
+                Expanded(child: KtvUI(), flex: 3)
+              ],
+            ))));
+  }
+}
+
+class Song {
+  String _name;
+  String _url;
+
+  Song(this._name, this._url) {}
+
+  String get name => _name;
+  String get url => _url;
+}
+
+// Inherited Widget for Video and KTV
+
+class _InheritedWidgetForApp extends InheritedWidget {
+  _InheritedWidgetForApp({
+    Key key,
+    @required Widget child,
+    @required this.data,
+  }) : super(key: key, child: child);
+
+  final AppInheritedWidgetState data;
+
+  @override
+  bool updateShouldNotify(_InheritedWidgetForApp oldWidget) {
+    return true;
+  }
+}
+
+class AppInheritedWidget extends StatefulWidget {
+  AppInheritedWidget({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  AppInheritedWidgetState createState() => new AppInheritedWidgetState();
+
+  static AppInheritedWidgetState of(BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(_InheritedWidgetForApp)
+            as _InheritedWidgetForApp)
+        .data;
+  }
+}
+
+class AppInheritedWidgetState extends State<AppInheritedWidget> {
+  List<Song> _songs = <Song>[];
+
+  List<Song> get songs => _songs;
+
+  void _addSong(Song reference, BuildContext context) {
+    setState(() {
+      _songs.add(reference);
+    });
+
+    final snackBar = SnackBar(
+      content: Text('Added ' + reference.name),
+      duration: Duration(seconds: 1),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  void _removeFirstSong() {
+    setState(() {
+      if (_songs.length > 0) _songs.removeAt(0);
+    });
+  }
+
+  void _pushSongToTop(Song song, int songIndex) {
+    setState(() {
+      _songs.removeAt(songIndex);
+      _songs.insert(0, song);
+    });
+  }
+
+  void _removeSong(int songIndex) {
+    setState(() {
+      _songs.removeAt(songIndex);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new _InheritedWidgetForApp(
+      data: this,
+      child: widget.child,
+    );
   }
 }
 
 // Video
 class VideoComponent extends StatefulWidget {
-  VideoComponent({Key key, this.videoUrl}) : super(key: key);
-
-  final String videoUrl;
+  VideoComponent({Key key}) : super(key: key);
 
   @override
   _VideoComponentState createState() => _VideoComponentState();
 }
 
 class _VideoComponentState extends State<VideoComponent> {
-  void playYoutubeVideo() {
-    FlutterYoutube.playYoutubeVideoByUrl(
-        apiKey: "AIzaSyAXhqkYvm0nn82rGDePq1l_ttw_T5Im1p0",
-        videoUrl: "https://www.youtube.com/watch?v=TmwgJ8I7QCI",
-        autoPlay: true,
-        fullScreen: false);
+  void playYoutubeVideo(AppInheritedWidgetState state) {
+    if (state._songs.length <= 0) {
+      // hello
+    } else {
+      FlutterYoutube.playYoutubeVideoByUrl(
+          apiKey: "AIzaSyAXhqkYvm0nn82rGDePq1l_ttw_T5Im1p0",
+          videoUrl: state._songs[0]._url,
+          autoPlay: true,
+          fullScreen: false);
+    }
+  }
+
+  void _nextVideo(AppInheritedWidgetState appState) {
+    appState._removeFirstSong();
   }
 
   @override
   Widget build(BuildContext context) {
+    final AppInheritedWidgetState state = AppInheritedWidget.of(context);
+
     return Container(
         decoration: BoxDecoration(color: Colors.blue[50]),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("This is a placeholder for video"),
-              RaisedButton(
-                child: Text("Press here to play video"),
-                onPressed: playYoutubeVideo,
-              ),
+              state._songs.length == 0
+                  ? Text("Nothing on playlist now!")
+                  : Text("Now playing " + state._songs[0]._name),
+              RaisedButton.icon(
+                  icon: Icon(Icons.play_circle_filled),
+                  onPressed: () => playYoutubeVideo(state),
+                  label: Text("Play Video")),
+              RaisedButton.icon(
+                  icon: Icon(Icons.queue_play_next),
+                  onPressed: () => _nextVideo(state),
+                  label: Text("Next Song"))
             ],
           ),
         ));
@@ -123,8 +219,7 @@ class _KtvUIState extends State<KtvUI> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return MyInheritedWidget(
-        child: CupertinoTabScaffold(
+    return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(CupertinoIcons.music_note),
@@ -138,65 +233,6 @@ class _KtvUIState extends State<KtvUI> with SingleTickerProviderStateMixin {
       tabBuilder: (context, index) => CupertinoPageScaffold(
             child: Center(child: index == 0 ? SelectSong() : Playlist()),
           ),
-    ));
-  }
-}
-
-// Inherited Widget
-
-class _MyInherited extends InheritedWidget {
-  _MyInherited({
-    Key key,
-    @required Widget child,
-    @required this.data,
-  }) : super(key: key, child: child);
-
-  final MyInheritedWidgetState data;
-
-  @override
-  bool updateShouldNotify(_MyInherited oldWidget) {
-    return true;
-  }
-}
-
-class MyInheritedWidget extends StatefulWidget {
-  MyInheritedWidget({
-    Key key,
-    this.child,
-  }) : super(key: key);
-
-  final Widget child;
-
-  @override
-  MyInheritedWidgetState createState() => new MyInheritedWidgetState();
-
-  static MyInheritedWidgetState of(BuildContext context) {
-    return (context.inheritFromWidgetOfExactType(_MyInherited) as _MyInherited)
-        .data;
-  }
-}
-
-class MyInheritedWidgetState extends State<MyInheritedWidget> {
-  /// List of Items
-  List<String> _items = <String>[];
-
-  /// Getter (number of items)
-  int get itemsCount => _items.length;
-
-  List<String> get items => _items;
-
-  /// Helper method to add an Item
-  void addItem(String reference) {
-    setState(() {
-      _items.add(reference);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new _MyInherited(
-      data: this,
-      child: widget.child,
     );
   }
 }
@@ -220,7 +256,7 @@ class _PlaylistState extends State<Playlist> {
         iconTheme: IconThemeData(color: Colors.black87));
   }
 
-  Widget _getSongsListView(MyInheritedWidgetState state) {
+  Widget _getSongsListView(AppInheritedWidgetState state) {
     return Center(
         child: ListView.builder(
       itemBuilder: (context, position) {
@@ -233,21 +269,32 @@ class _PlaylistState extends State<Playlist> {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        Text(state.items[position]),
-                        IconButton(
-                          icon: Icon(Icons.drag_handle),
-                          onPressed: () {},
-                        )
+                        Text(state.songs[position].name),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              IconButton(
+                                icon: Icon(Icons.arrow_upward),
+                                onPressed: () => state._pushSongToTop(
+                                    state.songs[position], position),
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () => state._removeSong(position),
+                              )
+                            ],
+                          ),
+                        ),
                       ],
                     ))));
       },
-      itemCount: state.items.length,
+      itemCount: state.songs.length,
     ));
   }
 
   @override
   Widget build(BuildContext context) {
-    final MyInheritedWidgetState state = MyInheritedWidget.of(context);
+    final AppInheritedWidgetState state = AppInheritedWidget.of(context);
 
     return Scaffold(
       appBar: _getAppBar("Playlist"),
@@ -273,32 +320,67 @@ class _SelectSongState extends State<SelectSong> {
   final Map _songData = {
     "categories": {
       "Meow Language": {
-        "The Meowttens": {
-          "Meowing Under the Moon": "www.google.com",
-          "Purr and Fur": "www.google.com",
-          "Santa Claws is Rolling to Town": ""
-        }
+        "The Meowttens": [
+          Song("Meowing Under the Moon", "www.google.com"),
+          Song("Purr and Fur", "www.google.com"),
+          Song("Santa Claws is Rolling to Town", "")
+        ]
       },
       "Chinese": {
-        "GEM": {
-          "光年之外": "https://www.youtube.com/watch?v=T4SimnaiktU",
-          "那一夜": "https://www.youtube.com/watch?v=ugVNDvnDDpA",
-          "倒数": "https://www.youtube.com/watch?v=ma7r2HGqwXs"
-        },
-        "Hebe Tien": {
-          "你就不要想起我": "https://www.youtube.com/watch?v=GsKbnsUN2RE",
-          "魔鬼中的天使": "https://www.youtube.com/watch?v=na_xv5iFt2Y",
-          "寂寞寂寞就好": "https://www.youtube.com/watch?v=DyFIzKYQQYE"
-        },
-        "JJ Lin": {},
-        "Kimberlyn Chen": {}
+        "GEM": [
+          Song("光年之外", "https://www.youtube.com/watch?v=T4SimnaiktU"),
+          Song("那一夜", "https://www.youtube.com/watch?v=ugVNDvnDDpA"),
+          Song("倒数", "https://www.youtube.com/watch?v=ma7r2HGqwXs")
+        ],
+        "Hebe Tien": [
+          Song("你就不要想起我", "https://www.youtube.com/watch?v=GsKbnsUN2RE"),
+          Song("魔鬼中的天使", "https://www.youtube.com/watch?v=na_xv5iFt2Y"),
+          Song("寂寞寂寞就好", "https://www.youtube.com/watch?v=DyFIzKYQQYE")
+        ],
+        "JJ Lin": [
+          Song("曹操", "https://www.youtube.com/watch?v=3J_bkgexrE8"),
+          Song("不死之身", "https://www.youtube.com/watch?v=h03_hs_QbEE"),
+          Song("美人鱼", "https://www.youtube.com/watch?v=jdf3gxFP0F8"),
+          Song("就是我", "https://www.youtube.com/watch?v=GLtLjBS0cs0")
+        ],
+        "Kimberlyn Chen": [
+          Song("爱你", "https://www.youtube.com/watch?v=4QoVMJN-nFs")
+        ]
       },
-      "Korean": {"Ailee": {}, "IU": {}},
+      "Korean": {
+        "Ailee": [
+          Song("Reminiscing", "https://www.youtube.com/watch?v=Spo8fiFHQls"),
+          Song("Heaven", "https://www.youtube.com/watch?v=DQzK8II1qAU"),
+          Song("I will go to you like first snow",
+              "https://www.youtube.com/watch?v=xW7gKmeXhuQ"),
+          Song(
+              "I will show you", "https://www.youtube.com/watch?v=-Q-OmGsk4hM"),
+          Song("If you", "https://www.youtube.com/watch?v=R3xy0vbxNUs"),
+          Song("Rainy Day", "https://www.youtube.com/watch?v=qSFFDxKE4B4"),
+          Song("U&I", "https://www.youtube.com/watch?v=398DDVagbPI"),
+        ]
+      },
       "English": {
-        "Fall Out Boy": {},
-        "Taylor Swift": {},
-        "Maroon 5": {},
-        "Adele": {}
+        "Fall Out Boy": [
+          Song("Dance, Dance", "https://www.youtube.com/watch?v=bxxJ25-3QEA"),
+          Song("Sugar, we're going down",
+              "https://www.youtube.com/watch?v=d5lOB7Ibjhg"),
+        ],
+        "Taylor Swift": [
+          Song("Love Story", "https://www.youtube.com/watch?v=PDM-GcQ6NT0")
+        ],
+        "Maroon 5": [
+          Song("Animals", "https://www.youtube.com/watch?v=a5-tjIB9YBk"),
+          Song("She will be loved",
+              "https://www.youtube.com/watch?v=zJmVq8-p1CE"),
+        ],
+        "Adele": [
+          Song("Someone like you",
+              "https://www.youtube.com/watch?v=Cgdes6lFjzM"),
+          Song("All I Asked", "https://www.youtube.com/watch?v=QnChoAPlMyc"),
+          Song("When we were young",
+              "https://www.youtube.com/watch?v=vVGUM_e5zDA"),
+        ]
       }
     }
   };
@@ -338,7 +420,7 @@ class _SelectSongState extends State<SelectSong> {
   }
 
   Scaffold _getCategoriesView() {
-    Map categoriesMap = _songData["categories"];
+    Map categoriesMap = _songData["categories"]; //eg. Chinese, English...
     List<dynamic> keys = categoriesMap.keys.toList();
 
     return Scaffold(
@@ -347,7 +429,8 @@ class _SelectSongState extends State<SelectSong> {
   }
 
   Scaffold _getCategoryDetailView() {
-    Map categoryMap = _songData["categories"][_currPageType[0]];
+    Map categoryMap =
+        _songData["categories"][_currPageType[0]]; // eg. GEM, JJ ...
     List<dynamic> keys = categoryMap.keys.toList();
 
     return Scaffold(
@@ -355,9 +438,9 @@ class _SelectSongState extends State<SelectSong> {
         body: _getGridView(keys, _goToSinger));
   }
 
-  Scaffold _getSingerDetailView(MyInheritedWidgetState state) {
-    Map singerMap = _songData["categories"][_currPageType[0]][_currPageType[1]];
-    List<dynamic> keys = singerMap.keys.toList();
+  Scaffold _getSingerDetailView(AppInheritedWidgetState appState) {
+    List<Song> songs =
+        _songData["categories"][_currPageType[0]][_currPageType[1]];
 
     return Scaffold(
         appBar: _getAppBar(_currPageType[1], true),
@@ -373,17 +456,17 @@ class _SelectSongState extends State<SelectSong> {
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: <Widget>[
-                            Text(keys[position]),
+                            Text(songs[position].name),
                             IconButton(
                               icon: Icon(Icons.add),
                               onPressed: () {
-                                state.addItem(keys[position]);
+                                appState._addSong(songs[position], context);
                               },
                             )
                           ],
                         ))));
           },
-          itemCount: keys.length,
+          itemCount: songs.length,
         )));
   }
 
@@ -408,10 +491,10 @@ class _SelectSongState extends State<SelectSong> {
 
   @override
   Widget build(BuildContext context) {
-    final MyInheritedWidgetState state = MyInheritedWidget.of(context);
+    final AppInheritedWidgetState appState = AppInheritedWidget.of(context);
     if (_currPageType.length == 0) return _getCategoriesView();
     if (_currPageType.length == 1) return _getCategoryDetailView();
-    if (_currPageType.length == 2) return _getSingerDetailView(state);
+    if (_currPageType.length == 2) return _getSingerDetailView(appState);
     return _getNotFoundView();
   }
 }
